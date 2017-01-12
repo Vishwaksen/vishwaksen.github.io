6e5a5158b59918aeb73d7a3a93c30b727eac42fd
Vishwaksen <vishwaksen.mane@gmail.com>
2017-01-12 17:43:51 -0500

    footer styling

---
+++ script/script.js
@@ -0,0 +1,17 @@
+// Send Email
+document.getElementById("myForm").addEventListener('submit', sendEmail);
+
+function sendEmail(e){
+//Get form values
+var msg = document.getElementById("message").value;
+console.log(msg);
+
+// Prevent form from submitting
+e.preventDefault();
+}
+
+// View Resume 
+var view_resume = document.getElementById("view_resume");
+view_resume.onclick = function(){
+	window.open('https://drive.google.com/open?id=0B7I9_jGzeDDQS21QMFlJY0VJQkE','_blank');
+}
