c2972c6175bf28f9082376fc8bcf99ecf12c0bb9
Vishwaksen <vishwaksen.mane@gmail.com>
2017-01-22 01:54:10 -0500

    Added Email Functionality and Support

--- script/script.js
+++ script/script.js
@@ -2,20 +2,55 @@
   Author : Vishwaksen
 */
 
-// Send Email
-document.getElementById("myForm").addEventListener('submit', sendEmail);
+$(document).ready(function(){
+	myForm.trigger("reset");
+});
 
-function sendEmail(e){
-//Get form values
-var msg = document.getElementById("message").value;
-console.log(msg);
 
-// Prevent form from submitting
-e.preventDefault();
-}
-
-// View Resume 
+/*// View Resume 
 var view_resume = document.getElementById("view_resume");
 view_resume.onclick = function(){
-	window.open('https://drive.google.com/open?id=0B7I9_jGzeDDQS21QMFlJY0VJQkE','_blank');
-}
+	window.open('https://drive.google.com/open?i','_blank');
+}*/
+
+// Handles the E-mail Functionality
+$(document).ready(function(){
+	$(".send_mail_button").click(function(){
+		var myForm = $(".contact_form");
+		var from_name = $("#email_name").val();
+		var email_id = $("#email_id").val();
+		var email_content = $("#email_content").val();
+
+		if(from_name == ""){
+			alert("Please Enter your Name");
+		}
+		if(email_id == ""){
+			alert("Please Enter your Email ID");
+		}
+		if(email_content == ""){
+			alert("Please Enter your message before sending the Email");
+		}
+
+		var email_json = {
+			"to_name" : "Vishwaksen",
+			"from_name" : from_name,
+			"reply_to" : email_id,
+			"message_html" : email_content
+		}
+		var service_id="default_service";
+		var template_id="my_email_template";
+		emailjs.send("service_id","template_id");
+		myForm.find("button").text("Sending...");
+		emailjs.send(service_id,template_id,email_json)
+  			.then(function(){ 
+       		alert("Email Sent Successfully!");
+       myForm.find("button").text("Send");
+       myForm.trigger("reset");
+     }, function(err) {
+       alert("Send email failed!\r\n Response:\n " + JSON.stringify(err));
+       myForm.find("button").text("Send");
+       myForm.trigger("reset");
+    });
+  return false;
+	});
+});
